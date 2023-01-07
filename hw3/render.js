0d2f987025aeb0d7732903949971eab5a1d1e894
ruoxin030704 <113079505+ruoxin030704@users.noreply.github.com>
2023-01-08 05:55:31 +0800

    ruoxin030704

---
+++ hw3/render.js
@@ -0,0 +1,104 @@
+export function layout(title, content) {
+    return `
+    <html>
+    <head>
+      <title>${title}</title>
+      <style>
+        body {
+          padding: 80px;
+          font: 16px Helvetica, Arial;
+        }
+    
+        h1 {
+          font-size: 2em;
+        }
+    
+        h2 {
+          font-size: 1.2em;
+        }
+    
+        #posts {
+          margin: 0;
+          padding: 0;
+        }
+    
+        #posts li {
+          margin: 40px 0;
+          padding: 0;
+          padding-bottom: 20px;
+          border-bottom: 1px solid #eee;
+          list-style: none;
+        }
+    
+        #posts li:last-child {
+          border-bottom: none;
+        }
+    
+        textarea {
+          width: 500px;
+          height: 300px;
+        }
+    
+        input[type=text],
+        textarea {
+          border: 1px solid #eee;
+          border-top-color: #ddd;
+          border-left-color: #ddd;
+          border-radius: 2px;
+          padding: 15px;
+          font-size: .8em;
+        }
+    
+        input[type=text] {
+          width: 500px;
+        }
+      </style>
+    </head>
+    <body>
+      <section id="content">
+        ${content}
+      </section>
+    </body>
+    </html>
+    `
+  }
+  
+  export function list(posts) {
+    let list = []
+    for (let post of posts) {
+      list.push(`
+      <li>
+        <h2>${ post.title }</h2>
+        <p><a href="/post/${post.id}">閱讀</a></p>
+      </li>
+      `)
+    }
+    let content = `
+    <h1>私人日記</h1>
+    <p>你擁有 <strong>${posts.length}</strong> 篇日記</p>
+    <p><a href="/post/new">建立新日記</a></p>
+    <ul id="posts">
+      ${list.join('\n')}
+    </ul>
+    `
+    return layout('Posts', content)
+  }
+  
+  export function newPost() {
+    return layout('New Post', `
+    <h1>新日記</h1>
+    <p>創建新日記</p>
+    <form action="/post" method="post">
+      <p><input type="text" placeholder="Title" name="title"></p>
+      <p><textarea placeholder="Contents" name="body"></textarea></p>
+      <p><input type="submit" value="建立"></p>
+    </form>
+    `)
+  }
+  
+  export function show(post) {
+    return layout(post.title, `
+      <h1>${post.title}</h1>
+      <p>${post.body}</p>
+    `)
+  }
